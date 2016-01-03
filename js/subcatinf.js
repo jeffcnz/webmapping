6d8072e9875ffa1e22f51cfa22b81f0723f23908
jeffcnz <jeffjcooke@hotmail.com>
2016-01-04 11:24:07 +1300

    initial commit

---
+++ js/subcatinf.js
@@ -0,0 +1,103 @@
+var subCatInfo = [{
+  "Name": "Hawea",
+	"properties":{
+		"ID":"T14",
+		"StockEx":"No",
+		"Priority":"No"
+		}},
+  {"Name": "Kahahakuri",
+	"properties":{
+		"ID":"T3",
+		"StockEx":"Yes",
+		"Priority":"Yes"
+		}},
+  {"Name": "Maharakeke",
+	"properties":{
+		"ID":"T8",
+		"StockEx":"Yes",
+		"Priority":"No"
+		}},
+  {"Name": "Makara",
+	"properties":{
+		"ID":"T13",
+		"StockEx":"No",
+		"Priority":"No"
+		}},
+  {"Name": "Makeretu",
+	"properties":{
+		"ID":"T6",
+		"StockEx":"No",
+		"Priority":"No"
+		}},
+  {"Name": "Mangaonuku",
+	"properties":{
+		"ID":"T2",
+		"StockEx":"No",
+		"Priority":"No"
+		}},
+  {"Name": "Mangarara",
+	"properties":{
+		"ID":"T12",
+		"StockEx":"No",
+		"Priority":"No"
+		}},
+  {"Name": "Mangatarata",
+	"properties":{
+		"ID":"T9",
+		"StockEx":"No",
+		"Priority":"No"
+		}},
+  {"Name": "Mangamahaki",
+	"properties":{
+		"ID":"T10",
+		"StockEx":"No",
+		"Priority":"No"
+		}},
+  {"Name": "Papanui",
+	"properties":{
+		"ID":"T11",
+		"StockEx":"Yes",
+		"Priority":"Yes"
+		}},
+  {"Name": "Porangahau",
+	"properties":{
+		"ID":"T7",
+		"StockEx":"Yes",
+		"Priority":"Yes"
+		}},
+  {"Name": "Lower Tukituki Corridor",
+	"properties":{
+		"ID":"T16",
+		"StockEx":"No",
+		"Priority":"No"
+		}},
+  {"Name": "Upper Tukituki",
+	"properties":{
+		"ID":"T4",
+		"StockEx":"Yes",
+		"Priority":"No"
+		}},
+  {"Name": "Waipawa",
+	"properties":{
+		"ID":"T1",
+		"StockEx":"No",
+		"Priority":"No"
+		}},
+  {"Name": "Tukipo",
+	"properties":{
+		"ID":"T5",
+		"StockEx":"Yes",
+		"Priority":"No"
+		}},
+  {"Name": "Upper Tukituki Corridor",
+	"properties":{
+		"ID":"T15",
+		"StockEx":"No",
+		"Priority":"No"
+		}},
+  {"Name": "Makaroro",
+	"properties":{
+		"ID":"T17",
+		"StockEx":"No",
+		"Priority":"No"
+  }}];
